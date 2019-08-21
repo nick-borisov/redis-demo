cdc8d831a643168d550ba9f0b926658f1a519e00
Nick Borisov <n.borisov.ua@gmail.com>
2019-08-21 13:56:45 +0300

    add header markup

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
 <template>
   <div id="app">
-
+    <SiteHeader
+      logoImage="https://www.qbrobotics.com/wp-content/uploads/2018/03/sample-logo-170x150.png"
+      logoTitle="Organizer"
+      logoSubtitle="Home"
+      userEmail="gregory.murphy@example.com"
+      userName="Gregory Murphy"
+      userPhoto="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+    />
   </div>
 </template>
 
 <script>
 import './assets/scss/general.scss'
+import SiteHeader from './components/SiteHeader'
 
 export default {
   name: 'app',
   components: {
+    SiteHeader,
   }
 }
 </script>
---
+++ src/components/SiteHeader/index.js
@@ -0,0 +1,4 @@
+import SiteHeader from './SiteHeader.vue'
+import './style.scss'
+
+export default SiteHeader
